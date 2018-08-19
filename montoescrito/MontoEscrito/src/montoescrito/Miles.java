c8f253c79710bbcb5cdf115813551e961bae48f1
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-19 12:22:41 -0500

    parte2.

--- montoescrito/MontoEscrito/src/montoescrito/Miles.java
+++ montoescrito/MontoEscrito/src/montoescrito/Miles.java
@@ -8,7 +8,7 @@ class Miles {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
--- montoescrito/MontoEscrito/src/montoescrito/MontoEscrito.java
+++ montoescrito/MontoEscrito/src/montoescrito/MontoEscrito.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 
 
 class MontoEscrito {
-
-	
+     int texto;
+	 int numero;
      public int Miles (int Miles){
     	 int numero;
     	 
@@ -25,7 +25,11 @@ class MontoEscrito {
      }
      
      public int unidad(int unidad) {
-    	 
+    	 int numero=0;
+    	 int unidadEnTexto=0;
+    	 int uniada= numero %10;
+    	 numero = numero/10;
+    	 texto = unidadEnTexto;
     	 return unidad;
      }
 }   
