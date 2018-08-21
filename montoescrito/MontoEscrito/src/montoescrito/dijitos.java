024734836cf52a08691bbf900e651e6efd62da56
Anibal Toro <anibal.toro@utp.edu.co>
2018-08-20 21:53:25 -0500

    parte9.

--- montoescrito/MontoEscrito/src/montoescrito/dijitos.java
+++ montoescrito/MontoEscrito/src/montoescrito/dijitos.java
@@ -7,30 +7,30 @@ import org.junit.jupiter.api.Test;
 class dijitos {
 
 	@Test
-	/*void testcuatrodijitos() {
+	void testcuatrodijitos() {
 		MontoEscrito cuatrodijitos = new MontoEscrito();
 	    String valoresperado="nueve mil ochocientos veinte";
 	    String valorobtenido= cuatrodijitos.Concatenado(9820);
 	    assertEquals(valoresperado,valorobtenido);
-	}*/
-	/*void testtresdijitos() {
+	}
+	void testtresdijitos() {
 		MontoEscrito tresdijitos = new MontoEscrito();
 	    String valoresperado="doscientos trece";
 	    String valorobtenido= tresdijitos.Concatenado(213);
 	    assertEquals(valoresperado,valorobtenido);
 	    System.out.println(valorobtenido);
-	}*/
+	}
 	void testdosdijitos() {
 		MontoEscrito dosdijitos = new MontoEscrito();
 	    String valoresperado="trece";
 	    String valorobtenido= dosdijitos.Concatenado(13);
 	    assertEquals(valoresperado,valorobtenido);
 	}
-	/*void testundijitos() {
+	void testundijitos() {
 		MontoEscrito undijitos = new MontoEscrito();
 	    String valoresperado="tres";
 	    String valorobtenido= undijitos.Concatenado(3);
 	    assertEquals(valoresperado,valorobtenido);
-	    System.out.println(valorobtenido);*/
+	    System.out.println(valorobtenido);
 	}
-/*}*/
+}
